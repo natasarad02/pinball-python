9e8b625cc8d0b75356257cd7d6d5647854be6a0d
Natasa Radmilovic <nata.radmilovic@gmail.com>
2024-01-28 23:49:45 +0100

    prebacila sam palice u linije

--- flipper.py
+++ flipper.py
@@ -27,6 +27,13 @@ class Line:
         self.b_y = b_y
         self.color = color
         self.width = width
+        self.distance = math.sqrt((self.b_x - self.a_x) ** 2 + (self.b_y - self.a_y) ** 2)
+        self.rotation_angle = math.asin((self.b_y - self.a_y)/self.distance)
+        self.tmp_a_x = self.a_x
+        self.tmp_a_y = self.a_y
+        self.tmp_b_y = self.b_y
+        self.tmp_b_x = self.b_x
+
 
 
     def draw(self):
@@ -48,13 +55,33 @@ class Line:
 
             normal_vector = pygame.math.Vector2(-line_vector.y, line_vector.x)
             incident_angle = point_vector.angle_to(normal_vector)
-            if point_vector.y > line_vector.y:
-                incident_angle = 360 - incident_angle
+
+            if incident_angle < 0:
+                incident_angle += 360
 
         # print(distance <= ball.radius)
             return math.radians(incident_angle), distance <= ball.radius
         else:
             return None, False
+    def rotate_left(self):
+        self.b_x = self.a_x + self.distance#math.tan(self.rotation_angle) * (self.b_y - self.a_y)
+        self.b_y = self.a_y
+        self.draw()
+    def rotate_reset_left(self):
+        self.b_x = self.tmp_b_x
+        self.b_y = self.tmp_b_y
+
+    def rotate_right(self):
+        self.a_x = self.b_x - self.distance#math.tan(self.rotation_angle) * (self.a_y - self.b_y)
+        self.a_y = self.b_y
+        self.draw()
+    def rotate_reset_right(self):
+        self.a_x = self.tmp_a_x
+        self.a_y = self.tmp_a_y
+
+
+
+
 
 
 class Circle:
@@ -97,19 +124,20 @@ class Ball(Circle):
         #self.positions = [self.x_pos, self.y_pos]
         #self.speed = [self.x_speed, self.y_speed]
 
-    def update(self, flippers, circle_obstacles, line):
+    def update(self, flippers, circle_obstacles):
         # Collision with obstacle_circle
-        incident_angle, isCollided = line.is_collided(self)
-        #print(incident_angle)
+        for i in range(len(flippers)):
+            incident_angle, isCollided = flippers[i].is_collided(self)
+            #print(incident_angle)
 
-        if isCollided == True:
+            if isCollided == True:
 
-            reflection_angle =  incident_angle + math.radians(180)
-            print(math.degrees(reflection_angle))
-            self.direction = [math.cos(reflection_angle), math.sin(reflection_angle)]
-            length = math.sqrt(self.direction[0] ** 2 + self.direction[1] ** 2)
-            reflection = [self.direction[0] / length, self.direction[1] / length]
-            self.direction = reflection
+                reflection_angle =  incident_angle + math.radians(180)
+                print(math.degrees(reflection_angle))
+                self.direction = [math.cos(reflection_angle), math.sin(reflection_angle)]
+                length = math.sqrt(self.direction[0] ** 2 + self.direction[1] ** 2)
+                reflection = [self.direction[0] / length, self.direction[1] / length]
+                self.direction = reflection
 
 
 
@@ -139,7 +167,7 @@ class Ball(Circle):
 
 
 
-
+        '''
         for i in range(len(flippers)):
 
             if flippers[i].is_rotated == True:
@@ -169,7 +197,7 @@ class Ball(Circle):
             else:
                 pass
 
-
+         '''
 
 
         # Collision with window edges
@@ -195,97 +223,7 @@ class Ball(Circle):
 
 
 
-class Left_Flipper:
 
-    def __init__(self, x, y, height, width, pivot, HEIGHT, WIDTH, starting_angle = -45):
-        self.x = x
-        self.y = y
-        self.h = height
-        self.w = width
-        self.pivot = pivot
-        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
-        pygame.draw.rect(self.image, 'purple', (0, 0, width, height))
-        self.angle = starting_angle
-        self.is_rotated = False
-
-       # self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
-        #self.brick_screen = pygame.display.set_mode([width, height])
-
-    def update_left(self, elapsed_time):
-        rotation_speed = 100
-        self.angle += rotation_speed * 0.5
-    def update_reset(self, elapsed_time):
-        rotation_speed = 100
-        self.angle -= rotation_speed * 0.5
-
-    def draw_brick(self, color):
-        rotated_surface = pygame.transform.rotate(self.image, self.angle)
-        rotated_rect = rotated_surface.get_rect(topleft=(self.x, self.y))
-        screen.blit(rotated_surface, rotated_rect.topleft)
-
-    def rotate_left(self, timer):
-        while self.angle < 0:
-            elapsed_time = timer.tick(30) / 1000.0
-            self.update_left(elapsed_time)
-            #self.image.fill('purple')
-            #self.screen.fill('black')
-            self.is_rotated = True
-            self.draw_brick('purple')
-            pygame.display.flip()
-
-
-    def reset_rotation(self, timer):
-        while self.angle > -45:
-            elapsed_time = timer.tick(30) / 1000.0
-            self.update_reset(elapsed_time)
-            #self.image.fill('purple')
-           # self.screen.fill('black')
-            self.is_rotated = False
-            self.draw_brick('purple')
-            pygame.display.flip()
-
-class Right_Flipper:
-    def __init__(self, x, y, height, width, pivot, HEIGHT, WIDTH, starting_angle=-135):
-        self.x = x
-        self.y = y
-        self.h = height
-        self.w = width
-        self.pivot = pivot
-        self.flipper_surface = pygame.Surface((width, height), pygame.SRCALPHA)
-        pygame.draw.rect(self.flipper_surface, 'purple', (0, 0, width, height))
-        self.is_rotated = False
-        self.angle = starting_angle
-
-    def update_right(self, elapsed_time):
-        rotation_speed = 100
-        self.angle -= rotation_speed * 0.5
-
-    def update_reset(self, elapsed_time):
-        rotation_speed = 100
-        self.angle += rotation_speed * 0.5
-
-    def draw_brick(self, color):
-        rotated_surface = pygame.transform.rotate(self.flipper_surface, self.angle)
-        rotated_rect = rotated_surface.get_rect(topright=(self.x + self.w, self.y))
-        screen.blit(rotated_surface, rotated_rect.topleft)
-
-    def rotate_right(self, timer):
-        while self.angle > -180:
-            elapsed_time = timer.tick(30) / 1000.0
-            self.update_right(elapsed_time)
-            self.draw_brick('purple')
-            self.is_rotated = True
-            pygame.display.flip()
-           # print(self.y)
-
-
-    def reset_rotation(self, timer):
-        while self.angle < -135:
-            elapsed_time = timer.tick(30) / 1000.0
-            self.update_reset(elapsed_time)
-            self.is_rotated = False
-            self.draw_brick('purple')
-            pygame.display.flip()
 
 
 
@@ -306,10 +244,10 @@ circle_obstacle2 = Circle(380, 200, 50, 'green')
 circle_obstacle3 = Circle(550, 400, 50, 'green')
 circle_obstacles = [circle_obstacle1, circle_obstacle2, circle_obstacle3]
 
-line = Line(100, 700, 380, 850, 'red', 2)
+line = Line(100, 700, 380, 850, 'red', 6)
 timer = pygame.time.Clock()
-left_flipper = Left_Flipper(150, 1000, 35, 200, Vector2(150, 1000), HEIGHT, WIDTH)
-right_flipper = Right_Flipper(400, 1000, 35, 200, Vector2(500, 1000), HEIGHT, WIDTH)
+left_flipper = Line(100, 1000, 300, 1150, 'purple', 30)
+right_flipper = Line(480, 1150, 680, 1000, 'purple', 30)
 flippers = [left_flipper, right_flipper]
 run = True
 while run:
@@ -321,31 +259,34 @@ while run:
     circle_obstacle1.draw()
     circle_obstacle2.draw()
     circle_obstacle3.draw()
-    line.draw()
+    #line.draw()
      # Check collision with the right flipper
-    ball.update(flippers, circle_obstacles, line)
+    ball.update(flippers, circle_obstacles)
 
 
    # brick.update_pivot()
    # brick.draw_brick('purple')
-    left_flipper.draw_brick('purple')
-    right_flipper.draw_brick('purple')
+    left_flipper.draw()
+    right_flipper.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-
-               left_flipper.rotate_left(timer)
+                left_flipper.rotate_left()
+              # left_flipper.rotate_left(timer)
             else:
-                right_flipper.rotate_right(timer)
+
+                right_flipper.rotate_right()
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                left_flipper.reset_rotation(timer)
+                left_flipper.rotate_reset_left()
+                #left_flipper.reset_rotation(timer)
             else:
-                right_flipper.reset_rotation(timer)
+
+               right_flipper.rotate_reset_right()
     #elapsed_time = timer.tick(30) / 1000.0
     #brick.update(elapsed_time)
 
